aec784939156020a5bd2230756fe210a01c9fbde
Lex Trofimov <daslef93@gmail.com>
2026-03-17 23:52:01 +0300

    стартер

---
+++ .storybook/main.ts
@@ -0,0 +1,18 @@
+import type { StorybookConfig } from '@storybook/web-components-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/web-components-vite",
+    "options": {}
+  }
+};
+export default config;
